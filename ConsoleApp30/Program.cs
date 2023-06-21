b67cc702d3420a0b3af4bafd34afd7ebaf6f6d63
StepanovMar <maste@DESKTOP-N7TT9HL>
2023-06-21 21:03:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание расширяемой хеш-таблицы
+        var table = new Dictionary<int, string>();
+
+        // Добавление элементов в таблицу
+        table.Add(1, "Яблоко");
+        table.Add(2, "Банан");
+        table.Add(3, "Вишня");
+
+        // Поиск элемента в таблице
+        int key = 3; // Сюда вписываем ключ 1,2,3 в зависимости, какая таблица нужна
+        if (table.TryGetValue(key, out string value))
+        {
+            Console.WriteLine($"Элемент с ключом {key} найден: {value}");
+        }
+        else
+        {
+            Console.WriteLine($"Элемент с ключом {key} не найден");
+        }
+
+        Console.ReadKey();
+    }
+}
